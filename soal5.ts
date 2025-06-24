f4fa098d41d1964260564f54e020357e7238830b
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-06-24 21:13:28 +0700

    Soal 5: konversi suhu derajat

---
+++ soal5.ts
@@ -0,0 +1,11 @@
+// Soal 5: Konversi derajat Celcius ke Fahrenheit
+// -----------------------------------------------
+// Rumus: F = (C × 9/5) + 32
+
+function celsiusToFahrenheit(c: number): number {
+  // rumus konversi celcius ke faranheit
+  return (c * 9) / 5 + 32;
+}
+
+console.log("Soal 5:", celsiusToFahrenheit(30)); // 86
+
