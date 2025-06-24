6df92d90060bb03cc02efe80e1954bbd6d6e45a3
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-06-24 21:20:43 +0700

    Soal 15: cari angka terbesar

---
+++ soal15.ts
@@ -0,0 +1,8 @@
+// Soal 15: Cari angka terbesar kedua dari array
+
+// function secondLargest(arr: number[]): number | null {
+//   const unique = [...new Set(arr)].sort((a, b) => b - a); // hapus duplikat dan urut menurun
+//   return unique[1] ?? null;
+// }
+
+// console.log("Soal 15:", secondLargest([4, 1, 7, 7, 5])); // 5
