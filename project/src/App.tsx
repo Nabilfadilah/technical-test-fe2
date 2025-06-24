1989b15beb8cb1afb77beef1d1c8d328a6c5d442
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-06-24 21:25:27 +0700

    Pemanggilan komponent lainnya

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+// import Soal21 from "./components/Soal21";
+import {TodoApp} from "./components/Soal21";
+import Soal22 from "./components/Soal22";
+import {ContactManager} from "./components/Soal23";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="soal21">
+        <p className="bgp">Soal 21:</p>
+        {/* <Soal21 /> */}
+        <TodoApp />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className="soal22">
+        <p className="bgp">Soal 22:</p>
+        <Soal22 />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className="soal23">
+        <p className="bgp">Soal 23:</p>
+        <ContactManager />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
